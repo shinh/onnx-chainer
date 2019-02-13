3d34d162f1429aaea1d1c3d58847160f94968304
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-14 02:07:16 +0900

    [OC2] Use simple functions to define ops

--- onnx_chainer/onnx_chainer2/functions/activation.py
+++ onnx_chainer/onnx_chainer2/functions/activation.py
@@ -6,34 +6,28 @@ import chainer.functions as F
 from onnx_chainer.onnx_chainer2 import converter
 
 
-@converter.generic_converter(1)
 def clipped_relu(gb, x, z):
     return gb.Clip([x], min=0.0, max=float(z))
 
 
-@converter.generic_converter(1)
 def elu(gb, x, alpha):
     return gb.Elu([x], alpha=float(alpha))
 
 
-@converter.generic_converter(1)
 def leaky_relu(gb, x, slope):
     return gb.LeakyRelu([x], alpha=float(slope))
 
 
-@converter.generic_converter(1)
 def log_softmax(gb, x, axis):
     assert axis == 1, 'LogSoftmax with axis != 1 is not supported yet.'
     return gb.LogSoftmax([x], axis=int(axis))
 
 
-@converter.generic_converter(1)
 def softmax(gb, x, axis):
     assert axis == 1, 'Softmax with axis != 1 is not supported yet.'
     return gb.Softmax([x], axis=int(axis))
 
 
-@converter.generic_converter(1)
 def softplus(gb, x, beta):
     assert beta == 1.0, 'Softplus with beta != 1.0 is not supported yet.'
     return gb.Softplus([x])
@@ -41,16 +35,16 @@ def softplus(gb, x, beta):
 
 def get_mapping():
     mapping = {
-        F.clipped_relu: clipped_relu,
-        F.elu: elu,
+        F.clipped_relu: converter.generic(clipped_relu, 1),
+        F.elu: converter.generic(elu, 1),
         F.hard_sigmoid: converter.unary('HardSigmoid'),
-        F.leaky_relu: leaky_relu,
-        F.log_softmax: log_softmax,
+        F.leaky_relu: converter.generic(leaky_relu, 1),
+        F.log_softmax: converter.generic(log_softmax, 1),
         F.prelu: converter.binary('PRelu'),
         F.relu: converter.unary('Relu'),
         F.sigmoid: converter.unary('Sigmoid'),
-        F.softmax: softmax,
-        F.softplus: softplus,
+        F.softmax: converter.generic(softmax, 1),
+        F.softplus: converter.generic(softplus, 1),
         F.tanh: converter.unary('Tanh'),
     }
     return mapping
