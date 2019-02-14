8d52bf06a3fdafa93c7db08be2b268e1ae9c834e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-14 19:59:47 +0900

    [OC2] Reshape before GEMM in linear

--- onnx_chainer/onnx_chainer2/functions/connection.py
+++ onnx_chainer/onnx_chainer2/functions/connection.py
@@ -1,3 +1,4 @@
+import numpy as np
 import chainer.functions as F
 from onnx_chainer.onnx_chainer2 import converter
 
@@ -24,6 +25,13 @@ def convolution_2d(gb, x, w, b, stride, pad, cover_all, dilate=1, groups=1):
 def linear(gb, x, w, b, n_batch_axes):
     assert n_batch_axes == 1, \
         'n_batch_axes != 1 for linear is not supported yet'
+
+    # TODO(hamaji): Support graphs with dynamic shapes.
+    x_shape = gb.shape(x)
+    if len(x_shape) != 2:
+        shape = gb.const(np.array([x_shape[0], np.prod(x_shape[1:])]))
+        x = gb.Reshape([x, shape]).output[0]
+
     if b is None:
         t = gb.Transpose([w], perm=[1, 0])
         return gb.MatMul([x, t.output[0]])
